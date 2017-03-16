b20e4d0e1a16f0f61b3fcd5a4b36543941bffa4d
Friendy <friendy.sin@lloydsbanking.com>
2017-03-16 11:48:45 +0000

    add react skeleton work

---
+++ src/components/List.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredients = [{"id":1, "text":"ham"}, {"id":2, "text":"cheese"}, {"id":3, "text":"potato"}, {"id":4, "text":"eggs"}];
+
+var List = React.createClass({
+  render: function() {
+    var listItem = ingredients.map(function(item) {
+      return <ListItem key={item.id} ingredient={item.text} />;
+    });
+    return (<ul>{listItem}</ul>);
+  }
+});
+
+module.exports = List;
